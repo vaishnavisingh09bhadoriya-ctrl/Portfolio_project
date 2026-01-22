c14d0217110d564575fe8204251b942dd7aab8d4
Vaishnavi singh <vaishnavisingh09bhadoriya@gmail.com>
2026-01-22 14:10:47 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,50 @@ import './App.css'
 
 
 function App() {
+
+function scrollToSection() {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+  
+}
+
+
   return (
-    <div className=" min-h-screen  font-serif text-gray-900 antialiased">
-      <div className=" max-w-6xl mx-auto px-6 py-10">
+    <div  className=" min-h-screen bg-linear-to-bl from-sky-200 to-white text-gray-900   antialiased">
+      <div className=" max-w-6xl mx-auto  ">
         
 
-        <header className="sticky top-0 z-50 bg-pastel-cream  flex  justify-between mb-6 ">
+        <header class="hidden" className=" sticky top-0 z-50  bg-linear-to-bl from-sky-200 to-white shadow-sm  flex px-5 h-10  justify-between mb-6 ">
           <div className="  flex items-center gap-3">
            <span className='font-extrabold text-lg'> VAISHNAVI</span>
           </div>
-          <div className=' flex justify-center gap-8 text-sm'>
-           <div className='text-shadow-lg'>home</div>
-           <div className='text-shadow-lg'>About</div>
-           <div className='text-shadow-lg'>contact me</div>
+          <div className=' flex justify-center items-center gap-8 text-sm'>
+           <div  className='nav-simple  text-shadow-lg'><button onClick={() => scrollToSection("home")}><a href="#home">Home</a></button></div>
+           <div  className='nav-simple  text-shadow-lg'><button  onClick={() => scrollToSection("about")}><a href="#about">About</a></button></div>
+           <div  className='nav-simple  text-shadow-lg'><button onClick={() => scrollToSection("contact")}><a href="#contact">Contact</a></button></div>
           </div>
 
           
         </header>
 
-        <main>
-          <section className="flex flex-col md:flex-row items-center justify-between gap-6 mb-8 px-6 py-2
+        <main  id="home">
+          <section className="flex flex-col md:flex-row items-center justify-between gap-6 mb-8 px-6 py-6
 ">
-<div className='relative  isolate bg-pastel-cream  h-64 w-64 '>
-                <img src='https://www.shutterstock.com/shutterstock/videos/3710958393/thumb/9.jpg?ip=x480'
-                className='absolute inset-0 w-full h-full object-cover bg-pastel-cream-500 mix-blend-multiply  ' alt='Picture'></img>
+<div className=' card relative isolate mb-5 h-64 w-64 '>
+     
+                
+                <img src='https://static.vecteezy.com/system/resources/thumbnails/044/448/931/small/cartoon-character-with-the-desk-working-concept-illustration-free-png.png'
+                className='absolute fade-bottom h-80 w-80 object-cover bg-transparen bg-blend-color' alt='Picture'></img>
               </div>
            
 
-            <div className=' font-extrabold text-6xl text-center px-5 py-4 '> Want to create with me?
+            <div className=' card font-extrabold text-6xl text-center px-5 py-4 '> Want to create with me?
               <p className='text-sm text-center m-8  py-7'>Hi! I'm a Frontend developer who creates modern, clean, interactive websites.</p>
              
               <div className='flex items-center justify-center space-x-4 m-8'>
-                 <button className='text-sm btn-simple'>let's connect</button>
-              <button className='text-sm btn-ghost'> see my work</button>
+                 <button className='text-sm btn-simple'> <a href='https://github.com/discussions' target='blank'>let's connect</a></button>
+              <button className='text-sm btn-ghost'> <a href='https://github.com/vaishnavisingh09bhadoriya-ctrl?tab=repositories' target='blank'>see my work</a></button>
               </div>
               </div>
               
@@ -42,32 +53,33 @@ function App() {
            
           </section>
 
-          <section id="about" className="mb-6 flex  items-center justify-center gap-6 px-6">
+          <section id="about" className=" card mb-6 flex  items-center justify-center gap-6 px-6">
            
              <div className='flex flex-col items-center justify-center mb-5 border-gray-300 border-2 rounded-lg px-4 shadow-lg'><div className='text-center m-5'><h1 className='font-bold text-3xl'>About me</h1></div>
-             <div className='text-wrap text-shadow-sm'> <p className='text-sm py-5 px-2'>I am a Aspiring frontend developer who wants to learn and develop websites that modern. I have done diploma in computer Application and web development. Currently pursuing MCA in AI and ML. I am currently expanding my knowledge of backend systems and modern development practices in the technology sector. </p></div></div>  
+             <div className='text-wrap text-shadow-sm'> <p className=' py-5 px-2 text-sm/6 md:text-lg/7'>I am a Aspiring frontend developer who wants to learn and develop websites that modern. I have done diploma in computer Application and web development. Currently pursuing MCA in AI and ML. I am currently expanding my knowledge of backend systems and modern development practices in the technology sector. </p></div></div>  
               
             
             
             
           </section>
 
-          <section id="projects" className="mb-6 px-6 flex flex-col items-center flex-wrap  justify-center overflow-x-scroll "> <div className='text-center m-8'><h1  className='font-bold text-3xl'>Skills</h1></div>
-            <div className='flex flex-col gap-6 md:flex-row   '>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">HTML</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">CSS</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">JavaScript</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">DOM</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">Responsive design</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">UI development</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">Git & Github</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200" >API handeling</div>
-            <div className=" mb-2 shadow-lg px-4 py-3 rounded-lg shadow-yellow-200">React</div></div>
+          <section  id="projects" className=" card mb-6 px-6  flex flex-col items-center   justify-center "> <div className='text-center m-8'><h1  className='font-bold text-3xl'>Skills</h1></div>
+            <div className='flex flex-wrap gap-6 md:flex-row justify-center '>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15 bg-linear-to-bl from-sky-200 to-white rounded-lg shadow-grey-100">HTML</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">CSS</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">JavaScript</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15 bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">DOM</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">Responsive design</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">UI development</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">Git & Github</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100" >API handeling</div>
+            <div className=" skills mb-2 shadow-lg px-4 py-3 w-50 h-15  bg-linear-to-bl from-sky-200 to-white text-center rounded-lg shadow-grey-100">React</div></div>
             
           </section>
 
-          <section id="contact" className=" flex flex-col items-center justify-center mb-8  border-gray-300 border-2 rounded-lg px-6 shadow-lg "> 
-            <div className='text-center m-8'><h2  className='font-bold text-3xl text-shadow-lg'>Describe your idea?</h2></div>
+          <section  className=" description flex flex-col items-center justify-center mb-8  border-gray-300 border-2 rounded-lg px-6 shadow-lg "> 
+            <div className='text-center m-8'><h2  className=' font-bold text-3xl text-shadow-lg'>Describe your <span className='text-sky-400'>idea?</span> </h2>
+            <p className='mt-2 text-sm'>This is a frontend-only project. The purpose is to demostrate UI design. The form is intentionally not functional</p></div>
             <div >
             <form className='flex flex-col items-center justify-center flex-wrap content-center'>
               <div><label for="name" >Name:</label>
@@ -87,7 +99,7 @@ function App() {
 
         </main>
 
-        <footer className="mt-8  text-sm   ">
+        <footer  id="contact" className=" mt-8  text-sm   ">
           <div className='text-center m-5'><h3 className='font-bold text-3xl text-shadow-lg px-3'>Social links</h3></div>
           <div className='flex gap-10 items-center justify-center py-5 px-3'>
           <div><a href='https://www.linkedin.com/in/vaishnavi-singh-75804a362?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target='blank'>LinkedIn</a></div>
